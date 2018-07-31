35b53ca8d5c7ed8962802c4b5c2dd2a86ec380d6
freemany <freeman_yam@hotmail.com>
2018-07-31 20:55:31 +1000

    lazy load path

--- todo/src/router/index.js
+++ todo/src/router/index.js
@@ -1,13 +1,18 @@
 import Router from 'vue-router'
-import TodoEditItem from './../components/TodoEditItem'
+// import TodoEditItem from './../components/TodoEditItem'
 // Views
 // const Dashboard = () => import('@/views/Dashboard')
+const TodoEditItem = () => import('@/components/TodoEditItem')
+
+// function loadView(view) {
+//   return () => import(/* webpackChunkName: "view-[request]" */ `@/components/${view}.vue`)
+// }
 
 export default new Router({
   mode: 'hash', // https://router.vuejs.org/api/#mode
   linkActiveClass: 'open active',
   scrollBehavior: () => ({ y: 0 }),
   routes: [
-    { path: '/edit/:id', component: TodoEditItem}
+    { path: '/edit/:id', component: TodoEditItem }
   ]
 });
